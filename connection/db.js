ff35b16ca23506e5ec8cafb2212aa64e4e44f150
farrel216 <farrel.arp@gmail.com>
2023-01-12 21:39:55 +0700

    authentication jwt

--- connection/db.js
+++ connection/db.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 // Mongo Atlas
-// mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser: true,
-// useUnifiedTopology: true});
+mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser: true,
+useUnifiedTopology: true});
 
 // local
-mongoose.connect(process.env.DATABASE_URL)
+// mongoose.connect(process.env.DATABASE_URL)
