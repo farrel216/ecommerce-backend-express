725b89272b7f44a8985ddd21b5fedfa5fb16807b
farrel216 <farrel.arp@gmail.com>
2023-01-21 17:22:56 +0700

    new api

--- routes/api.js
+++ routes/api.js
@@ -1,22 +1,38 @@
-var express = require('express');
+const express = require('express');
 const { register, login, logout } = require('../controllers/authController');
-var router = express.Router();
-const {getProduct, getDetailProduct} = require( '../controllers/productController');
+const { getCart, addCartItems, emptyCart } = require('../controllers/cartController');
+const { getCategory } = require('../controllers/categoryController');
+const { addOrder } = require('../controllers/orderController');
+const { getProduct, getDetailProduct, getProductByCategory } = require('../controllers/productController');
 const { getUserData } = require('../controllers/userController');
 const { refreshToken } = require('../middleware/refreshToken');
+const { verifyToken } = require('../middleware/verifyToken');
+
+const router = express.Router();
 
 // middleware
-router.get('/token', refreshToken)
+router.get('/token', refreshToken);
 
 // authentication
-router.post('/auth/register', register)
-router.post('/auth/login', login)
-router.delete('/auth/logout', logout)
+router.post('/auth/register', register);
+router.post('/auth/login', login);
+router.delete('/auth/logout', logout);
 
-router.get('/users/:id', getUserData)
+router.get('/users', verifyToken, getUserData);
 
 /* GET home page. */
-router.get('/product',getProduct)
-router.get('/product/detail/:productId',getDetailProduct)
+router.get('/product', getProduct);
+router.get('/product/category/:category', getProductByCategory);
+router.get('/product/detail/:productId', getDetailProduct);
 
+// Category
+router.get('/category', getCategory);
+
+// Cart
+router.get('/cart', verifyToken, getCart);
+router.post('/cart', verifyToken, addCartItems);
+router.delete('/cart', verifyToken, emptyCart);
+
+// Order
+router.post('/order', verifyToken, addOrder);
 module.exports = router;
