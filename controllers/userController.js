3dd7841568a688f62b0a45f2eb414fcbe79a80ec
farrel216 <farrel.arp@gmail.com>
2023-01-21 17:22:31 +0700

    add user schema

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,16 +1,18 @@
-const User = require("../models/userModel.js");
+const User = require('../models/userModel.js');
 
-const getUserData = async (req,res) =>{
-    const userId = req.params.id
-    User.findById(userId, (err,user)=>{
-            if(err){
-                res.status(500).send({message:"Gagal mendapatkan data user", error:err})
-            }
-            else{
-                res.status(200).send({message:"Berhasil mendapatkan data user", data:{email:user.email, name:user.name}})
-            }
-        })
-        
-}
+const getUserData = async (req, res) => {
+  const userId = req.id;
+  User.findById(userId, (err, user) => {
+    if (err) {
+      res.status(500).send({ message: 'Gagal mendapatkan data user', error: err });
+    } else {
+      res.status(200).send({ message: 'Berhasil mendapatkan data user', data: { email: user.email, name: user.name } });
+    }
+  });
+};
 
-module.exports = {getUserData}
+const editUserDate = async (req, res) => {
+  const userId = req.params.id;
+};
+
+module.exports = { getUserData };
--- models/userModel.js
+++ models/userModel.js
@@ -1,25 +1,33 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const User = mongoose.model("User", {
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    name: {
-        type: String,
-        required: true,
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    role: {
-        type: String,
-    },
-    refresh_token: {
-        type: String,
-    }
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  alamat: {
+    type: String,
+  },
+  nohp: {
+    type: String,
+  },
+  role: {
+    type: String,
+  },
+  refresh_token: {
+    type: String,
+  },
 });
+
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
